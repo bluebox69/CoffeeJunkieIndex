37ea245cb4c4e3a6e82f1020295a340950206ca3
Paul Kogler <paulkogler@MacBook-Pro-von-Paul.local>
2024-10-10 21:53:23 +0200

    Add switch case to print Result

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
@@ -18,6 +16,47 @@ public class Main {
         System.out.println(".... Calculating Junkie Index ....");
         int junkieIndex = calculateJunkieIndex(cupAmount, coffeeCups, coffeeTypeSelector.getCaffeineContent());
 
+        printResult(junkieIndex);
+        System.out.println("Programm finished");
+
+
+    }
+    private static void printResult(int junkieIndex) {
+        switch (junkieIndex){
+            case 1:
+                System.out.println("Junkie-Index: 1 You are very sober, Good!");
+                break;
+            case 2:
+                System.out.println("Junkie-Index: 2 You are sober, Go ahead!");
+                break;
+            case 3:
+                System.out.println("Junkie-Index: 3 You like Coffee, do you!");
+                break;
+            case 4:
+                System.out.println("Junkie-Index: 4 A moderate coffee lover, keep sipping!");
+                break;
+            case 5:
+                System.out.println("Junkie-Index: 5 Halfway to coffee nirvana, nice!");
+                break;
+            case 6:
+                System.out.println("Junkie-Index: 6 You're in the zone, just one more cup!");
+                break;
+            case 7:
+                System.out.println("Junkie-Index: 7 Caffeine enthusiast detected! Proceed with caution.");
+                break;
+            case 8:
+                System.out.println("Junkie-Index: 8 Serious coffee addiction, but we love it!");
+                break;
+            case 9:
+                System.out.println("Junkie-Index: 9 You might need an intervention, but who needs sleep?");
+                break;
+            case 10:
+                System.out.println("Junkie-Index: 10 Coffee is your spirit animal! Time to join a support group!");
+                break;
+            default:
+                System.out.println("Junkie-Index: Invalid level. Are you sure you drink coffee?");
+                break;
+        }
     }
 
     private static int calculateJunkieIndex(int cupAmount, int[] coffeeCups, double[] caffeineContent) {
